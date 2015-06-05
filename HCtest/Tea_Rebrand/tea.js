6186fe4a54922ab7b313a6096f17407488bab624
CJ Johnson <Fermentedbeats@gmail.com>
2015-06-05 09:46:24 -0700

    JS abandoned - img sizing adj

--- HCtest/Tea_Rebrand/tea.js
+++ HCtest/Tea_Rebrand/tea.js
@@ -17,15 +17,16 @@
 // 	}
 // });
 
+// var pur = document.getElementById("purchaseNow");
+// var h4s = pur.getElementsByTagName("h4");
+// var spins = pur.getElementsByTagName("i");
 
-var spins = document.getElementsByClassName("fa-spinner");
-
-	for (var i = 0; i < spins.length; i++) {
-spins[1].addEventListener("mouseover", function() {
-		spins[i].className += " fa-spin";
-		console.log('hi');
-	});
-};
+// 	for (var i = 0; i < spins.length; i++) {
+//        h4s[i].addEventListener("mouseover", function() {
+// 		spins[i].className += " fa-spin";
+// 		console.log('hi');
+// 	});
+// };
 
 // h5.addEventListener("mouseout", function() {
 
