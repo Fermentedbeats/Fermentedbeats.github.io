6bcadb508a7ba5126b1769aa9e00cbf9bfeabde9
CJ Johnson <Fermentedbeats@gmail.com>
2015-06-05 09:21:48 -0700

    new txtfield

--- HCtest/Tea_Rebrand/tea.js
+++ HCtest/Tea_Rebrand/tea.js
@@ -1,27 +1,40 @@
 
 
-var h5 = document.getElementById("purchaseNow");
-var ingred = document.getElementById("button");
+// var h5 = document.getElementById("purchaseNow");
 
-h5.addEventListener("mouseover", function() {
-	var spins = h5.getElementsByClassName("fa-spinner");
+// h5.addEventListener("mouseover", function() {
+// 	var spins = h5.getElementsByClassName("fa-spinner");
+// 	for (var i = 0; i < spins.length; i++) {
+// 		spins[i].className += " fa-spin";
+// 	}
+// });
+
+// h5.addEventListener("mouseout", function() {
+
+// 	var spins = h5.getElementsByClassName("fa-spinner");
+// 	for (var i = 0; i < spins.length; i++) {
+// 		spins[i].classList.remove("fa-spin");
+// 	}
+// });
+
+
+var spins = document.getElementsByClassName("fa-spinner");
+
 	for (var i = 0; i < spins.length; i++) {
+spins[1].addEventListener("mouseover", function() {
 		spins[i].className += " fa-spin";
-	}
-});
+		console.log('hi');
+	});
+};
 
-h5.addEventListener("mouseout", function() {
+// h5.addEventListener("mouseout", function() {
 
-	var spins = h5.getElementsByClassName("fa-spinner");
-	for (var i = 0; i < spins.length; i++) {
-		spins[i].classList.remove("fa-spin");
-	}
-});
+// 	var spins = h5.getElementsByClassName("fa-spinner");
+// 	for (var i = 0; i < spins.length; i++) {
+// 		spins[i].classList.remove("fa-spin");
+// 	}
+// });
 
-ingred.addEventListener("mouseover", function() {
-	
-	
-})
 
 
 
